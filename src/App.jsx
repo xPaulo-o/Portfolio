464c6c12330e0f107a3c3c77c25dcc4a87c222be
xPaulo-o <pauloaugusto2355@gmail.com>
2026-01-10 17:26:00 -0300

    erros

--- src/App.jsx
+++ src/App.jsx
@@ -23,10 +23,12 @@ function App() {
     const link = document.querySelector("link[rel~='icon']");
     if (link) {
       link.href = `${import.meta.env.BASE_URL}logo.png`;
+      link.type = "image/png";
     } else {
       const newLink = document.createElement("link");
       newLink.rel = "icon";
       newLink.href = `${import.meta.env.BASE_URL}logo.png`;
+      newLink.type = "image/png";
       document.head.appendChild(newLink);
     }
   }, []);
