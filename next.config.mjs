df2d61e1665d8176693642177948278c5cfa0b22
xPaulo-o <pauloaugusto2355@gmail.com>
2026-02-05 13:49:07 -0300

    correções

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/Portfolio',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
